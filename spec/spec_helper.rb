0dccaaec60cf3a829933cc11a90103c2d4b4744c
Lisa Folliard & Brigham Thompson <student@epicodus.com>
2015-10-22 14:42:06 -0700

    add a comment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,7 @@ SimpleCov.start
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.after(:suite) do
-    FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"])
+    FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"]) # this is needed for Paperclip RSpec testing
   end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
